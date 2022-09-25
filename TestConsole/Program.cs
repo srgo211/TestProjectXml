44ecf06ceb85abda7ad8cea79a98ec9bf096ccd6
srgo211 <novikovsn87@gmail.com>
2022-09-25 23:00:08 +0300

    Get XMl position

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,2 +1,172 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+
 Console.WriteLine("Hello, World!");
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+Encoding.GetEncoding("windows-1254");
+
+string path = @"D:\VisualStudio\XmlWPF\ReplaseCore\Эталон 14.xml";
+
+XML xml = new XML();
+var x = xml.GetXmlElements(path);
+
+
+Console.WriteLine(x);
+
+#region Models
+public class Positions
+{
+
+	public string Caption { get; set; }
+	public string Number { get; set; }
+	public string Code { get; set; }
+	public string Units { get; set; }
+	public string SysID { get; set; }
+	public string PriceLevel { get; set; }
+	public string Quantity { get; set; }
+	public string Options { get; set; }
+	public string PzSync { get; set; }
+	public string Vr2001 { get; set; }
+	public string Mass { get; set; }
+
+	public Quantity Quantitys { get; set; }
+	public Resources Resources { get; set; }
+	public Koefficient Koefficient { get; set; }
+	public List<WorksList> Works { get; set; }
+
+}
+
+
+public class Quantity
+{
+	public int Fx { get; set; }
+	public int Result { get; set; }
+}
+
+#region Resources
+public class Resources
+{
+	public Tzr[] TZR { get; set; }
+	public Tzm[] TZM { get; set; }
+	public Mch[] MCH { get; set; }
+	public Mat[] MAT { get; set; }
+}
+
+public class Tzr : BaseResources, IBaseResources { }
+
+public class Tzm : BaseResources, IBaseResources { }
+
+public class Mch : BaseResources, IBaseResources { }
+
+public class Mat : BaseResources, IBaseResources
+{
+	public string Options { get; set; }
+	public string Mass { get; set; }
+	public List<Koefficient> Koefficients { get; set; }
+}
+#endregion
+
+public class PriceCurr
+{
+	public string Value { get; set; }
+
+	public string Comment { get; set; }
+
+	public string ZM { get; set; }
+}
+
+public class Koefficient
+{
+	public string Caption { get; set; }
+	public string Options { get; set; }
+	public int Level { get; set; }
+}
+
+public class WorksList
+{
+	public string Caption { get; set; }
+}
+
+public class CustomNPCurr
+{
+	public string Nacl { get; set; }
+	public string NaclMask { get; set; }
+	public string Plan { get; set; }
+	public string PlanMask { get; set; }
+}
+
+
+public abstract class BaseResources
+{
+	public string Caption { get; set; }
+
+	public string Code { get; set; }
+
+	public string Units { get; set; }
+
+	public string Quantity { get; set; }
+
+	public string WorkClass { get; set; }
+
+	public PriceCurr PriceCurr { get; set; }
+}
+
+
+public interface IBaseResources
+{
+	string Caption { get; set; }
+
+	string Code { get; set; }
+
+	string Units { get; set; }
+
+	string Quantity { get; set; }
+
+	string WorkClass { get; set; }
+
+	PriceCurr PriceCurr { get; set; }
+}
+
+#endregion
+
+
+#region  Расширение
+public static class Expansion
+{
+	public static string PathFileXML { get; set; }
+	public static void AddDic(this Dictionary<string, List<string>> dic, string key, string value)
+	{
+		if (dic == null) return;
+
+		if (dic.ContainsKey(key))
+		{
+			var res = dic[key];
+			res.Add(value);
+		}
+		else
+		{
+			List<string> newList = new List<string>();
+			newList.Add(value);
+			dic.Add(key, newList);
+
+		}
+	}
+
+
+	private static string GetValueAttribute(Dictionary<string, List<string>> dicAttributes, string nameAttribute)
+	{
+		if (dicAttributes.ContainsKey(nameAttribute))
+		{
+			string result = String.Join("\n", dicAttributes[nameAttribute]);
+			return result;
+		}
+		else return default;
+
+	}
+}
+#endregion
+
+
+
+
+
---
+++ TestConsole/XML.cs
@@ -0,0 +1,183 @@
+﻿using System.Xml.Linq;
+using System.Xml.XPath;
+
+public class XML
+{
+	const string Quantity = "Quantity";
+	const string Tzr = "Tzr";
+	const string Tzm = "Tzm";
+	const string Mch = "Mch";
+	const string Mat = "Mat";
+	const string WorkList = "WorkList";
+
+	public List<BaseResources> GetBase(XElement elPosition, string xpath, string teg)
+	{
+		XElement[] els = elPosition.XPathSelectElements(xpath)?.ToArray();
+
+		if (els == null || els.Count() <= 0) return default;
+
+		int sizeArr = els.Count();
+		List<BaseResources> baseResources = new List<BaseResources>();
+
+
+
+		BaseResources baseRes = null;
+
+		switch (teg)
+		{
+			case Tzr: baseRes = new Tzr(); break;
+			case Tzm: baseRes = new Tzm(); break;
+			case Mch: baseRes = new Mch(); break;
+			case Mat: baseRes = new Mat(); break;
+		}
+
+
+		for (int i = 0; i < sizeArr; i++)
+		{
+			XElement el = els[i];
+			var dicAtr = GetDicAtributes(el);
+
+
+			baseRes.Caption = GetValueAttribute(dicAtr, "Caption");
+			baseRes.Code = GetValueAttribute(dicAtr, "Code");
+			baseRes.Units = GetValueAttribute(dicAtr, "Units");
+			baseRes.Quantity = GetValueAttribute(dicAtr, "Quantity");
+			baseRes.WorkClass = GetValueAttribute(dicAtr, "WorkClass");
+
+
+			XElement elPrice = el.XPathSelectElement(".//PriceCurr");
+			var dicAtrPrices = GetDicAtributes(elPrice);
+			PriceCurr price = new PriceCurr()
+			{
+				Value = GetValueAttribute(dicAtrPrices, "Value"),
+				Comment = GetValueAttribute(dicAtrPrices, "Comment"),
+				ZM = GetValueAttribute(dicAtrPrices, "ZM"),
+			};
+
+			baseRes.PriceCurr = price;
+			baseResources.Add(baseRes);
+
+		}
+		return baseResources;
+	}
+
+
+
+
+	public List<Positions> GetXmlElements(string pathFileXml)
+	{
+
+		XElement root = XElement.Load(pathFileXml);
+		//получаем все позиции
+		XElement[] rootElements = root.Descendants("Position").ToArray();
+
+		if (rootElements == null || rootElements.Length <= 0) return default;
+
+
+		List<Positions> positions = new List<Positions>();
+
+		//перебор POSITION
+		for (int i = 0; i < rootElements.Length; i++)
+		{
+
+			XElement elPosition = rootElements[i];
+
+			Positions position = new Positions();
+
+			List<Quantity> quantitys = new List<Quantity>();
+			List<WorksList> worksLists = new List<WorksList>();
+
+
+			//получаем все атрибуты Position
+			var dicAtrPosition = GetDicAtributes(elPosition);
+			//Console.WriteLine(dicAtrPosition);
+			//Console.WriteLine($"\n***********");
+			//Console.WriteLine(elPosition.Name);
+
+			//Атрибуты
+			position.Caption = GetValueAttribute(dicAtrPosition, "Caption");
+			position.Number = GetValueAttribute(dicAtrPosition, "Number");
+			position.Code = GetValueAttribute(dicAtrPosition, "Code");
+			position.Units = GetValueAttribute(dicAtrPosition, "Units");
+			position.SysID = GetValueAttribute(dicAtrPosition, "SysID");
+			position.PriceLevel = GetValueAttribute(dicAtrPosition, "PriceLevel");
+			position.Quantity = GetValueAttribute(dicAtrPosition, "Quantity");
+			position.Options = GetValueAttribute(dicAtrPosition, "Options");
+			position.PzSync = GetValueAttribute(dicAtrPosition, "PzSync");
+			position.Vr2001 = GetValueAttribute(dicAtrPosition, "Vr2001");
+			position.Mass = GetValueAttribute(dicAtrPosition, "Mass");
+
+
+
+			position.Resources = GetResources(elPosition);
+
+
+			positions.Add(position);
+
+		}
+
+
+		return positions;
+
+
+	}
+
+
+	Resources GetResources(XElement elPosition)
+	{
+		Resources resources = new Resources();
+
+		List<BaseResources> tzrs = GetBase(elPosition, ".//Tzr", Tzr);
+		List<BaseResources> tzms = GetBase(elPosition, ".//Tzm", Tzm);
+		List<BaseResources> mchs = GetBase(elPosition, ".//Mch", Mch);
+		List<BaseResources> mats = GetBase(elPosition, ".//Mat", Mat);
+
+		resources.TZR = tzms != null ? tzrs.Select(r => (Tzr)r)?.ToArray() : null;
+		resources.TZM = tzms != null ? tzms.Select(r => (Tzm)r)?.ToArray() : null;
+		resources.MCH = mchs != null ? mchs.Select(r => (Mch)r)?.ToArray() : null;
+		resources.MAT = mats != null ? mats.Select(r => (Mat)r)?.ToArray() : null;
+
+
+		//Console.WriteLine(resources);
+		return resources;
+
+	}
+
+
+	public static Dictionary<string, List<string>> GetDicAtributes(XElement el)
+	{
+		if (el == null) return default;
+		Dictionary<string, List<string>> dicAttributes = new Dictionary<string, List<string>>();
+		foreach (XAttribute atr in el?.Attributes())
+		{
+			Console.WriteLine($"----> {atr.Name} = {atr?.Value} ");
+			try
+			{
+				dicAttributes.AddDic(atr.Name.ToString(), atr?.Value);
+			}
+			catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+		}
+
+		return dicAttributes;
+	}
+
+	public static string GetValueAttribute(Dictionary<string, List<string>> dicAttributes, string nameAttribute)
+	{
+		if (dicAttributes == null || dicAttributes.Count <= 0) return default;
+
+		if (dicAttributes.ContainsKey(nameAttribute))
+		{
+			string result = String.Join("\n", dicAttributes[nameAttribute]);
+			return result;
+		}
+		else return default;
+
+	}
+}
+
+
+
+
+
+
